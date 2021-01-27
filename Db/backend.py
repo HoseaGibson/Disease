0de7f8d3c699f0c16bfcdc01fa502b2a3aed6544
HoseaGibson <Hosea.Gibs@outlook.com>
2021-01-27 10:42:06 -0600

    added sql for clients

--- Db/backend.py
+++ Db/backend.py
@@ -1,6 +1,7 @@
 import sqlite3
 from tkinter import messagebox as ms
 
+
 def createUserDb():
     with sqlite3.connect('users.db') as db:
         c = db.cursor()
@@ -8,6 +9,24 @@ def createUserDb():
                   lastname TEXT NOT NULL,
                   username TEXT NOT NULL,
                   password TEXT NOT NULL);""")
+    db.commit()
+    db.close()
+
+
+def clientTableDb():
+    with sqlite3.connect('MODEL/clients.db') as db:
+        c = db.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS client(firstname TEXT NOT NULL,
+                    lastname TEXT NOT NULL,
+                    birthdate TEXT NOT NULL,
+                    address TEXT NOT NULL,
+                    postal TEXT NOT NULL,
+                    phone TEXT NOT NULL,
+                    symptom1 TEXT NOT NULL,
+                    symptom2 TEXT NOT NULL,
+                    symptom3 TEXT NOT NULL,
+                    symptom4 TEXT NOT NULL,
+                    symptom5 TEXT NOT NULL);""")
     db.commit()
     db.close()
 
@@ -39,3 +58,20 @@ def addUserAccountDb(first, last, username, password):
         con.execute(insertSql, [first, last, username, password])
         db.commit()
 
+
+def insertPatient(first, last, dob, add, zipcode, phone, sym1, sym2, sym3, sym4, sym5):
+    with sqlite3.connect('clients.db') as db:
+        con = db.cursor()
+
+    findSql = 'SELECT firstname, lastname FROM client WHERE firstname =? and lastname=? '
+    con.execute(findSql, [first, last])
+    f = con.fetchall()
+    if f:
+        ms.showerror('User already exist')
+    insertSql = 'INSERT INTO client(firstname, lastname, birthdate, address, postal, phone, symptom1, symptom2, ' \
+                'symptom3, symptom4, symptom5) VALUES(?,?,?,?,?,?,?,?,?,?) '
+    con.execute(insertSql,
+                [first, last, dob, add, zipcode, phone, sym1, sym2, sym3, sym4,
+                 sym5])
+    db.commit()
+    db.close()
