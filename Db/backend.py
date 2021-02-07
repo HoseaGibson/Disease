a9cc5760cd7a60120f424902b922f532a8b46651
HoseaGibson <Hosea.Gibs@outlook.com>
2021-02-07 07:08:29 -0600

    fixed backend functions

--- Db/backend.py
+++ Db/backend.py
@@ -1,4 +1,6 @@
 import sqlite3
+from tkinter import *
+
 from tkinter import messagebox as ms
 
 
@@ -31,13 +33,28 @@ def clientTableDb():
     db.close()
 
 
-def loginUserDb(username, password):
+def loginUserDb(username, password, clear1, clear2, screen):
+    from Screens.clients import client
     with sqlite3.connect('users.db') as db:
         con = db.cursor()
 
+    u = username
+    p = password
+
     sql = 'SELECT * FROM user WHERE username = ? and password = ?'
     con.execute(sql, [username, password])
-    con.fetchall()
+    results = con.fetchall()
+
+    if u == "" or p == "":
+        ms.showwarning('All entries with (*) are required ')
+    if results:
+        clear1.delete(0, END)
+        clear2.delete(0, END)
+        screen.destroy()
+        client()
+
+    else:
+        ms.showerror('Credentials incorrect')
     db.commit()
     db.close()
 
@@ -74,20 +91,45 @@ def insertPatient(first, last, dob, add, zipcode, phone, sym1, sym2, sym3, sym4,
     db.commit()
     db.close()
 
-def viewAll():
+
+def viewAll(screen):
     with sqlite3.connect('clients.db') as db:
-        c = db.cursor()
+        con = db.cursor()
     sql = 'SELECT *, oid FROM client'
-    c.execute(sql)
-    records = c.fetchall()
+    con.execute(sql)
+    records = con.fetchall()
     printRd = ""
     for record in records:
-        printRd += str(record[0]) + " " + str(record[1]) + " " + str(record[2]) + " " + str(record[3]) + " " + str(
-            record[4]) + " " + str(record[5]) + " " + str(record[6]) + " " + str(record[7]) + " " + str(
-            record[8]) + " " + str(record[9]) + '\n'
-    queryViewLabel = Label(self.master, text=self.printRd)
+        printRd += str(record) + "\n"
+        # printRd += str(record[0]) + " " + str(record[1]) + " " + str(record[2]) + " " + str(record[3]) + " " + str(
+        # record[4]) + " " + str(record[5]) + " " + str(record[6]) + " " + str(record[7]) + " " + str(
+        # record[8]) + " " + str(record[9]) + " " + str(record[10]) + " " + str(record[11]) + '\n'
+        queryViewLabel = Label(screen, text=printRd)
 
-    queryViewLabel.grid(row=8, column=0, columnspan=2)
+        queryViewLabel.grid(row=8, column=0, columnspan=2)
+
+    db.commit()
+    db.close()
+
+
+def searchIndividualQuery(first, last, screen):
+    with sqlite3.connect('clients.db') as db:
+        con = db.cursor()
+    if first == "" or last == "":
+        errorMess = Label(screen, text="Please enter a name and last name")
+        errorMess.grid(row=10, column=0, padx=10, sticky=W)
+    else:
+        sql = 'SELECT * FROM client WHERE firstname = ? and lastname = ?'
+        con.execute(sql, [(first), (last)])
+        records = con.fetchall()
+        printSearch = ""
+
+        for recs in records:
+            printSearch += str(recs[0]) + " " + str(recs[1]) + " " + str(recs[2]) + " " + str(recs[3]) + " " + str(
+                recs[4]) + " " + str(recs[5]) + " " + str(recs[6]) + " " + str(recs[7]) + " " + str(
+                recs[8]) + " " + str(recs[9]) + " " + str(recs[10]) + '\n'
+        querySearchLabel = Label(screen, text=printSearch)
+        querySearchLabel.grid(row=10, column=0, columnspan=2)
 
     db.commit()
     db.close()
